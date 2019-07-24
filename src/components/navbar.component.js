fe3fe95b1b10a5246136507b85e1ed2475beb386
Amrit Pal Singh <amritpalsingha4@gmail.com>
2019-07-23 20:55:52 -0400

    add navbar

---
+++ src/components/navbar.component.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import logo from "../incible-logo.png";
+import {Link} from "react-router-dom"
+
+const NavItem = (props) => {
+    return (<li className="nav-item">
+        <Link to={props.href} className="nav-link">
+            {props.children}
+        </Link>
+    </li>)
+};
+
+class NavigationBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <Link className="navbar-brand" to="/">
+                    <img src={logo} className="w-logo" alt=""/>
+                </Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+                        aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarColor01">
+                    <ul className="navbar-nav mr-auto">
+                        <NavItem href={"/"}>
+                            Home <span className="sr-only">(current)</span>
+                        </NavItem>
+                        <NavItem href={"/incidents"}>
+                            Incidents
+                        </NavItem>
+                        <NavItem href={"/features"}>
+                            Features
+                        </NavItem>
+                        <NavItem href={"/pricing"}>
+                            Pricing
+                        </NavItem>
+                        <NavItem href={"/about"}>
+                            About
+                        </NavItem>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default NavigationBar;
