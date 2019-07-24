cc1bfe658214a911a17281381f24cb950c948176
Amrit Pal Singh <amritpalsingha4@gmail.com>
2019-07-23 20:56:40 -0400

    add navbar to landing page

--- src/components/landing.page.component.js
+++ src/components/landing.page.component.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export const LandingPage = () => {
     return (
-        <div className="container d-flex justify-align-center">
-            <h2>Landing Page</h2>
+        <div>
+            <NavigationBar />
+            <h2 className="pt-2 pb-2">Landing Page</h2>
         </div>
     );
 };
