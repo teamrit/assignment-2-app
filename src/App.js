24b3a2852923741e794f207bc9a1bdddd21bd0e0
Amrit Pal Singh <amritpalsingha4@gmail.com>
2019-07-28 22:04:46 -0400

    add sign in component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import { Provider } from "react-redux";
 import {LandingPage} from "./components/landing.page.component";
-import {SignIn} from "./components/signin.component";
+import SignIn from "./components/signin.component";
 import SignUp from "./components/signup.component";
 import store from "./redux/store";
-import IncidentsList from "./components/incidents.component";
+import IncidentsList from "./components/incidents.list";
+import connect from "react-redux/es/connect/connect";
+import Logout from "./components/logout.component";
+import {loadUserTokenFromStorage} from "./redux/actions/users.action";
 
-function App() {
-  return (
-    <div className="App">
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/login/" component={SignIn} />
-        <Route path="/signup/" component={SignUp} />
-        <Route path="/incidents/" component={IncidentsList} />
-    </div>
-  );
+class AppI extends React.Component {
+
+    componentDidMount() {
+        if (this.props.loadUser()) {
+            // window.location.pathname = "/incidents";
+        }
+
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <Route path="/" exact component={LandingPage} />
+                <Route path="/login/" component={SignIn} />
+                <Route path="/logout" component={Logout} />
+                <Route path="/signup/" component={SignUp} />
+                <Route path="/incidents/" component={IncidentsList} />
+            </div>
+        );
+    }
 }
+
+export const App = connect((state => state), {loadUser: loadUserTokenFromStorage})(AppI);
 
 const routedApp = () => (
     <Provider store={store}>
