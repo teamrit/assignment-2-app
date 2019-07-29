d051032826c07d33f63721f717eec359c0adbef6
Amrit Pal Singh <amritpalsingha4@gmail.com>
2019-07-29 04:22:55 -0400

    add more cases for incidents

--- src/redux/reducers/incidents.reducer.js
+++ src/redux/reducers/incidents.reducer.js
@@ -1,5 +1,24 @@
-function incidents(state = {}, action) {
-    return state;
+import {INCIDENT} from "../constants";
+
+const initialState = {
+    incidents: [],
+    fetchDate: null,
+    error: {}
+};
+
+function incidentReducer(state = initialState, action) {
+    switch (action.type) {
+        case INCIDENT.GET.SUCCESS:
+            return Object.assign({}, state, {
+                incidents: action.payload
+            });
+        case INCIDENT.GET.FAILURE:
+            return Object.assign({}, state, {
+                error: {title: action.payload}
+            });
+        default:
+            return state
+    }
 }
 
-export default incidents;
+export default incidentReducer;
