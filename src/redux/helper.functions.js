81c227c8d61f1c2410475a6a86cd761a28ed437d
Amrit Pal Singh <amritpalsingha4@gmail.com>
2019-07-29 04:23:11 -0400

    add helper functions for dates and api calls

--- src/redux/helper.functions.js
+++ src/redux/helper.functions.js
@@ -1,3 +1,27 @@
 export const resolveHost = (url) => {
-    return `localhost:8000${url}`;
+    return `http://localhost:8000${url}`;
+};
+
+export const getDayName = date => {
+    const dayName = date.toLocaleDateString('en-US', { weekday: 'long' });
+    if (dayName) {
+        return dayName.substring(0,3);
+    }
+};
+
+export const getMonthName = date => {
+    const month = date.toLocaleString('default', { month: 'long' });
+    return month
+};
+
+/**
+ *
+ * @param dateString - Date String is the string representation coming from the backend
+ */
+export const beautifyDate = dateString => {
+    if (dateString) {
+        const date = new Date(dateString);
+        return `${getDayName(date)}, ${date.getDate()} ${getMonthName(date)}, ${date.getFullYear()}`;
+    }
+    return ""
 };
