a14d6b87c927b08d625fe021c74418df46b1b2c2
Amrit Pal Singh <amritpalsingha4@gmail.com>
2019-07-29 14:57:25 -0400

    add boilerplate landing page

--- src/components/landing.page.component.js
+++ src/components/landing.page.component.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import NavigationBar from "./navbar.component";
+import {Jumbotron, Button} from "react-bootstrap";
+
+export const Feature = () => {
+  return (
+      <div className="border rounded p-3">
+          <img src="https://cdn4.iconfinder.com/data/icons/project-management-5-3/65/247-512.png" alt="" className="img-thumbnail"/>
+          Hello
+      </div>
+  )
+};
 
 export const LandingPage = () => {
     return (
         <div>
             <NavigationBar />
-            <h2 className="pt-2 pb-2">Landing Page</h2>
+            <Jumbotron>
+                <h1 className="t-b mb-1">Welcome to Incible!</h1>
+                <p>
+                    Incident management made easy. <br/>
+                </p>
+                <h4 className='t-b mt-3'>
+                    The invincible incident management software written by professionals.
+                </h4>
+                <p className="mt-3">
+                    <Button variant="primary">Learn more</Button>
+                </p>
+            </Jumbotron>
+            <div className="container">
+                <div className="row">
+                    <div className="col-3 p-2">
+                        <Feature />
+                    </div>
+                    <div className="col-3 p-2">
+                        <Feature />
+                    </div>
+                    <div className="col-3 p-2">
+                        <Feature />
+                    </div>
+                    <div className="col-3 p-2">
+                        <Feature />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
