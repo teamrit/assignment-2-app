72618abb21aad701025564996fa496eed14ac791
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2019-07-30 23:41:56 -0400

    added content and Ui enhancements on landing page

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -3,7 +3,7 @@ import logo from "../incible-logo.png";
 import {Link} from "react-router-dom"
 
 const NavItem = (props) => {
-    return (<li className={`nav-item navi ${props.className} ${props.href == window.location.pathname && 'nav-a'}`}>
+    return (<li className="nav-item">
         <Link to={props.href} className="nav-link">
             {props.children}
         </Link>
@@ -11,20 +11,21 @@ const NavItem = (props) => {
 };
 
 export const NavItemIcon = ({icon = ''}) => {
-  return (
-      <React.Fragment>
-          <i className={`fas ${icon}`}/>&nbsp;{" "}
-      </React.Fragment>
-  )
-};
+    return (
+        <React.Fragment>
+            <i className={`fas ${icon}`}/>&nbsp;{" "}
+        </React.Fragment>
+    )
+  };
 
 class NavigationBar extends Component {
     render() {
-        const {isLoggedIn} = this.props;
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <Link className="navbar-brand" to="/">
-                    <img src={logo} className="w-logo" alt=""/>
+                    <div>
+                        <i class="fas fa-italic"></i>ncible
+                    </div>
                 </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
                         aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,33 +35,19 @@ class NavigationBar extends Component {
                 <div className="collapse navbar-collapse" id="navbarColor01">
                     <ul className="navbar-nav mr-auto">
                         <NavItem href={"/"}>
-                            <NavItemIcon icon={'fa-home'} />
                             Home <span className="sr-only">(current)</span>
                         </NavItem>
                         <NavItem href={"/incidents"}>
-                            <NavItemIcon icon={'fa-indent'} />
                             Incidents
                         </NavItem>
-                        <NavItem href={"/users"}>
-                            <NavItemIcon icon={'fa-user-friends'} />
-                            Users
-                        </NavItem>
                         <NavItem href={"/features"}>
-                            <NavItemIcon icon={'fa-star'} />
                             Features
                         </NavItem>
                         <NavItem href={"/pricing"}>
-                            <NavItemIcon icon={'fa-dollar-sign'} />
                             Pricing
                         </NavItem>
                         <NavItem href={"/about"}>
-                            <NavItemIcon icon={'fa-info-circle'} />
                             About
-                        </NavItem>
-                    </ul>
-                    <ul className={"navbar-nav float-right"}>
-                        <NavItem className={"float-right"} href={"/logout"}>
-                            <NavItemIcon icon={'fa-sign-out-alt'} />Logout
                         </NavItem>
                     </ul>
                 </div>
