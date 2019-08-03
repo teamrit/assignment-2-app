4f25e26bdacb0dab77833f3405dc5e884ac3221a
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-08-02 23:06:00 -0400

    revert last change

--- src/redux/actions/users.action.js
+++ src/redux/actions/users.action.js
@@ -159,8 +159,8 @@ export function getUsers() {
 export function editUser(newData) {
     return (dispatch, getState) => {
         if (loadUserTokenFromStorage()) {
-            const {_id} = newData;
-            const request = putAuthorized(resolveHost(`/user/${_id}`), getToken(), newData);
+            const {id} = newData;
+            const request = putAuthorized(resolveHost(`/user/${id}`), getToken(), newData);
             request.then(({ data }) => {
                 dispatch({ type: USER.EDIT_PROFILE.SUCCESS, payload: data });
             }).catch(error => {
