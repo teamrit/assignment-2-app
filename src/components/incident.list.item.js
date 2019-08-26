d1f676df9df951a5b01519945e9a4a723d0fa10a
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-08-26 16:20:33 -0400

    ui refactor for incidents list (list mode)

--- src/components/incident.list.item.js
+++ src/components/incident.list.item.js
@@ -7,7 +7,7 @@ import {Link} from "react-router-dom";
 
 export const IncidentListItem = ({incident= {} , deleteHandler}) => {
     return (
-        <div key={incident._id} className={"border p-4 rounded mt-3 text-align-left container text-left bg-eggshell shadow-lg"}>
+        <div key={incident._id} className={"p-4 mt-3 container"}>
             <h5 className="t-b">
                 <Link to={`/incident/${incident._id}/details`}>
                     <div className="d-inline">
@@ -24,11 +24,11 @@ export const IncidentListItem = ({incident= {} , deleteHandler}) => {
                 <Dropdown.Divider />
             </div>
             <div className="p-2">
-                <Link to={`/incident/${incident._id}/edit`} className="btn btn-sm mr-2 btn-info btn-sm brr t-b">
+                <Link to={`/incident/${incident._id}/edit`} className="btn btn-sm mr-2 btn-info btn-sm t-b">
                     <NavItemIcon icon="fa-edit"/>
                     Edit
                 </Link>
-                <Button type="submit" className="btn-sm bg-hiphop mr-2 brr t-b" onClick={deleteHandler}>
+                <Button type="submit" className="btn-sm mr-2 t-b" onClick={deleteHandler}>
                     <NavItemIcon icon="fa-trash"/>
                     Delete
                 </Button>
--- src/components/incident.table.row.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const IncidentTableRow = () => {
-    return <div>Table row</div>
-};
